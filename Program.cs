4f1732086a493e5f8f9882881f32e5c022182fad
Lauren Richert <lsrichert@gmail.com>
2018-08-22 13:58:05 -0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace exercise1List
+{
+    class Program
+    {
+        static void Main()
+        {
+            // 1. In the Main method, place the following code
+            List<string> planetList = new List<string>(){"Mercury", "Mars"};
+
+            // 2.  Add() Jupiter and Saturn at the end of the list.
+            planetList.Add("Jupiter");
+            planetList.Add("Saturn");
+            
+            // 3. Create another List that contains that last two planet of our solar system.
+            List<string> planetListNew = new List<string>(){"Neptune", "Uranus"};
+
+            // 4. User Insert() to add Earth, and Venus in the correct order.
+
+            Console.WriteLine(planetList);
+                    }
+    }
+}
