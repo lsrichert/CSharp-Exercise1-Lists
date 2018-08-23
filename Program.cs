d3f990ea5326de9a827968374d72e2dc4fd2c250
Lauren Richert <lsrichert@gmail.com>
2018-08-22 21:28:01 -0500

    some consoles showing up now but not all

--- Program.cs
+++ Program.cs
@@ -27,17 +27,28 @@ namespace exercise1List
             // 5. Use Add() again to add Pluto to the end of the list.
             planetList.Add("Pluto");
 
+            foreach (string planet in planetList)
+            {
+                Console.WriteLine(planet);
+            }
+
             // 6. Now that all the planets are in the list, slice the list using GetRange() in order to extract 
             // the rocky planets into a new list called rockyPlanets.
             List<string> rockyPlanets = planetList.GetRange(0, 4);
+
+            foreach (string rockyPlanet in rockyPlanets)
+            {
+                Console.WriteLine(rockyPlanets);
+            }
 
             // 7. Being good amateur astronomers, we know that Pluto is now a dwarf planet, so use the Remove() method 
             // to eliminate it from the end of planetList.
             planetList.Remove("Pluto");
 
-            foreach (string item in planetList);
-             Console.WriteLine(planetList);
-
+            // foreach (string planet in planetList);
+            // {
+            //     Console.WriteLine(planetList);
+            // }
           }
 
     }
