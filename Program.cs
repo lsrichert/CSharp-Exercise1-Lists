517376fd775aa5d6687795951b4ad94fbb6ae6d3
Lauren Richert <lsrichert@gmail.com>
2018-08-22 21:11:42 -0500

    did steps 1-7 but not consoling correctly still

--- Program.cs
+++ Program.cs
@@ -5,21 +5,40 @@ namespace exercise1List
 {
     class Program
     {
-        static void Main()
+        static void Main(string[] args)
         {
-            // 1. In the Main method, place the following code
-            List<string> planetList = new List<string>(){"Mercury", "Mars"};
+            // In the Main method, place the following code
+            List<string> planetList = new List<string>() {"Mercury", "Mars"};
 
-            // 2.  Add() Jupiter and Saturn at the end of the list.
+            // 1.  Add() Jupiter and Saturn at the end of the list.
             planetList.Add("Jupiter");
             planetList.Add("Saturn");
-            
-            // 3. Create another List that contains that last two planet of our solar system.
-            List<string> planetListNew = new List<string>(){"Neptune", "Uranus"};
+
+            // 2. Create another List that contains that last two planet of our solar system.
+            List<string> planetListNew = new List<string>(){"Uranus", "Neptune"};
+
+            // 3. Combine the two lists by using AddRange().
+            planetList.AddRange(planetListNew);
 
             // 4. User Insert() to add Earth, and Venus in the correct order.
+            planetList.Insert(3, "Earth");
+            planetList.Insert(2, "Venus");
 
-            Console.WriteLine(planetList);
-                    }
+            // 5. Use Add() again to add Pluto to the end of the list.
+            planetList.Add("Pluto");
+
+            // 6. Now that all the planets are in the list, slice the list using GetRange() in order to extract 
+            // the rocky planets into a new list called rockyPlanets.
+            List<string> rockyPlanets = planetList.GetRange(0, 4);
+
+            // 7. Being good amateur astronomers, we know that Pluto is now a dwarf planet, so use the Remove() method 
+            // to eliminate it from the end of planetList.
+            planetList.Remove("Pluto");
+
+            foreach (string item in planetList);
+             Console.WriteLine(planetList);
+
+          }
+
     }
 }
